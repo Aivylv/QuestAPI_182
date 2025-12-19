d0aad31c47995648882b652424083791abd98cfb
aivylv <lyvialarasati@gmail.com>
2025-12-20 00:20:31 +0700

    membuat sealed interface StatusUiSiswa untuk merepresentasikan status Loading, Success, dan Error

--- app/src/main/java/com/example/activity8_questapi/viewmodel/HomeVIewModel.kt
+++ app/src/main/java/com/example/activity8_questapi/viewmodel/HomeVIewModel.kt
@@ -10,3 +10,9 @@ import com.example.activity8_questapi.repositori.RepositoryDataSiswa
 import kotlinx.coroutines.launch
 import retrofit2.HttpException
 import java.io.IOException
+
+sealed interface StatusUiSiswa {
+    data class Success(val siswa: List<DataSiswa> = listOf()) : StatusUiSiswa
+    object Error : StatusUiSiswa
+    object Loading : StatusUiSiswa
+}
