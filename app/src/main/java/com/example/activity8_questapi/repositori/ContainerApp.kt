7ff199ca02090f06440712d69dcaa9b567db2621
aivylv <lyvialarasati@gmail.com>
2025-12-19 23:57:10 +0700

    mengatur base URL dan konfigurasi OkHttpClient dengan logging interceptor

--- app/src/main/java/com/example/activity8_questapi/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/activity8_questapi/repositori/ContainerApp.kt
@@ -12,3 +12,14 @@ import retrofit2.Retrofit
 interface ContainerApp {
     val repositoryDataSiswa: RepositoryDataSiswa
 }
+
+class DefaultContainerApp : ContainerApp {
+    private val baseUrl = "http://10.0.2.2/umyTI/"
+
+    val logging = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    val klien = OkHttpClient.Builder()
+        .addInterceptor(logging)
+        .build()
