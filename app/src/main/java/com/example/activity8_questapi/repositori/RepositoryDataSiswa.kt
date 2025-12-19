261e5a35ff4e9cb1655008bf431adb9f224f70b0
aivylv <lyvialarasati@gmail.com>
2025-12-19 23:50:36 +0700

    mendefinisikan interface RepositoryDataSiswa sebagai abstraksi untuk akses data siswa

--- app/src/main/java/com/example/activity8_questapi/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/activity8_questapi/repositori/RepositoryDataSiswa.kt
@@ -1,2 +1,8 @@
 package com.example.activity8_questapi.repositori
 
+import com.example.activity8_questapi.apiservice.ServiceApiSiswa
+import com.example.activity8_questapi.modeldata.DataSiswa
+interface RepositoryDataSiswa {
+    suspend fun getDataSiswa(): List<DataSiswa>
+    suspend fun postDataSiswa(dataSiswa: DataSiswa): retrofit2.Response<Void>
+}
