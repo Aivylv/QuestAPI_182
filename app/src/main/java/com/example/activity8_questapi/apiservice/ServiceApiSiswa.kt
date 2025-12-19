3f01c9e06ea338b2bf2cd9841b5e2046e2ac385e
aivylv <lyvialarasati@gmail.com>
2025-12-19 23:40:59 +0700

    menyiapkan package dan mengimpor library Retrofit untuk keperluan API

---
+++ app/src/main/java/com/example/activity8_questapi/apiservice/ServiceApiSiswa.kt
@@ -0,0 +1,7 @@
+package com.example.activity8_questapi.apiservice
+
+import com.example.activity8_questapi.modeldata.DataSiswa
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+
