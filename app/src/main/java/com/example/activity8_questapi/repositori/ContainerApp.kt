92d54988df24a2153a76ddb448823f648099ea5d
aivylv <lyvialarasati@gmail.com>
2025-12-19 23:55:45 +0700

    mendefinisikan interface ContainerApp sebagai kontrak untuk manajemen dependensi

---
+++ app/src/main/java/com/example/activity8_questapi/repositori/ContainerApp.kt
@@ -0,0 +1,14 @@
+package com.example.activity8_questapi.repositori
+
+import android.app.Application
+import com.example.activity8_questapi.apiservice.ServiceApiSiswa
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+
+interface ContainerApp {
+    val repositoryDataSiswa: RepositoryDataSiswa
+}
